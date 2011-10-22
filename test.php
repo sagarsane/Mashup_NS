518314535d7734d45eac5077af78788d0fc2d2f0
Sagar Sane <sagar2217@gmail.com>
2011-10-22 15:15:25 -0400

    More testing files...

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+
+	
+	$msg = "Hi Ankita, Chinmay: This message is sent from OUR NEW SMS API!!:)M sorry for sending the SMS, but I thought its the best way to let you guys knw this way:D";
+	$params = array( 'action' => 'create', 
+					 'token' => '086410b5705c344e8cc1b0bb042b6a4583cc689966ca86c36e59439e2c085db55a85eb0eacca6b9f6a60911f',
+					'to' => '9196751528',
+					 'msg' => $msg);
+	$query = "http://api.tropo.com/1.0/sessions?" . http_build_query( $params );
+	$xml = file_get_contents($query);
+	echo $xml;
+?>
