48a7d398ea72a82ea8128f6c69e204133bdc8944
Sagar Sane <sagar2217@gmail.com>
2011-11-26 21:30:36 -0500

    Started Generic Page. Integration with the New UI to be done

--- System/getResults.php
+++ System/getResults.php
@@ -1,20 +1,20 @@
-<?php
-
-	$searchTerm = $_POST['searchTerm'];
-	$i = 0;
-	for($i = 0; $i < 5;$i++)
-		getResults($i);
-	
-	function getResults($i){
-	print '<br/> <br/>';
-	print '<div id="list_u'.$i.'" align="center">
-			<table border="1" width="80%" height="20%">
+<?php	
+	require 'init_DB.php';
+	function getResults_($row,$i)
+	{
+		//$name = addslashes($row[1]);
+		
+		//$name= (object)$name1;
+		
+		print '<br/> <br/>';
+		print '<div id="list_u'.$i.'" align="center">
+				<table border="1" width="80%" height="20%">
 				<tr>
 					<td>
 					<table align="center">
 					<tr>
 						<td>
-						<a href="indivi.html"><h2>North Carolina State University</h2></a>
+						<a href="indivi.html?city='.$row[2].'&fees='.$row[3].'&link='.$row[4].'&gre='.$row[5].'"><h2>Rank: '.$row[0].' - '.$row[1].'</h2></a>
 						</td>
 					</tr>
 					</table>
@@ -29,11 +29,11 @@
 						</td>
 						<td width="30"></td>
 						<td align="center">
-						<img src="helpline.jpg" height="30" width="100"/>
+						<button name="call'.$i.'" id="call'.$i.'" height="30" width="100" onclick = "call_helpline('.$row[6].','.$i.')">Call Helpline: '.$row[6].'</button>
 						</td>
 						<td width="30"></td>
 						<td align="center">
-						<a href="subscribe.html" onclick="return popitup("subscribe.html")" >Subscribe</a>
+						<a href="subscribe.html?univ_name='.$row[1].'" onclick="return popitup("subscribe.html")" >Subscribe</a>
 						</td>
 						<td align="center">
 						
@@ -42,10 +42,32 @@
 					</table>
 					</td>
 					<td height="100" width="200" id="mapdiv'.$i.'" align = "center">
-					<img src="maps.jpg" height="30" width="100" onclick = "initialize('.$i.')"/>
+					<img src="maps.jpg" height="30" width="100" onclick = "initialize_map('.$i.',&quot;'.$row[1].'&quot;)"/>
 					</td>
 				</tr>
 			</table>
 			</div>';
 	}
+	
+	$q ="";
+	
+	if($_POST['top10']==1 && $_POST['cs']==0)//only top10
+	{
+		$q = "select * from `university_db` where `rank` between 1 and 10";
+	}
+	else if($_POST['top10'] == 0 && $_POST['cs'] == 1) // only CS
+	{
+		$q = "select * from `university_db` where `dept` = '1'";
+	}
+	else if($_POST['top10'] == 1 && $_POST['cs'] ==1 )//both
+	{
+		$q = "select * from `university_db`  where (`rank` between 1 and 10) and (`dept` = 1)";
+	}	
+	$res = mysql_query($q) or die('Error, query failed : ' . mysql_error());
+	$i = 0;
+	while( $row = (mysql_fetch_row( $res ) )){
+			getResults_($row,$i);
+			$i++;
+	}	
+	
 ?>
